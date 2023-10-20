98cc7996934a422143278e62084086cf3de773af
ship0000n <shipon0920063869@gmail.com>
2023-10-20 14:08:35 -0700

    8 finish

--- _printf.c
+++ _printf.c
@@ -18,7 +18,7 @@ va_start(ap, format);
 
 if (!format || (format[0] == '%' && !format[1]))
 return (-1);
-if (format[0] == '%' && format[1] == ' ' && !format[2]);
+if (format[0] == '%' && format[1] == ' ' && !format[2])
 return (-1);
 for (p = (char *)format; *p; p++)
 {
